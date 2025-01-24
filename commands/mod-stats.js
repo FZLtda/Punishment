72bf718b9069e924b5649e0c32192c7f29817e5a
FuncZero Dev <39081573-yfunczero@users.noreply.replit.com>
2025-01-24 15:45:31 +0000

    Atualização para melhorias no projeto

--- commands/mod-stats.js
+++ commands/mod-stats.js
@@ -68,10 +68,10 @@ module.exports = {
         .setTitle('<:emoji_48:1332357299339005974> Estatísticas de Moderação')
         .setColor('Blue')
         .addFields(
-          { name: '<:emoji_49:1332357944888524850> Total de Ações', value: `${totalActions.count || 0}`, inline: true },
-          { name: '<:emoji_49:1332356624869757058> Ações por Tipo', value: typeStats || 'Nenhuma ação registrada.', inline: true },
-          { name: '<:emoji_47:1323358397684387900> Top Moderadores', value: moderatorStats || 'Nenhuma ação registrada.', inline: false },
-          { name: '<:recente:1332355743592091780> Ações Recentes', value: recentStats || 'Nenhuma ação registrada.', inline: false }
+          { name: '<:emoji_49:1332357944888524850> | Total de Ações', value: `${totalActions.count || 0}`, inline: true },
+          { name: '<:emoji_49:1332356624869757058> | Ações por Tipo', value: typeStats || 'Nenhuma ação registrada.', inline: true },
+          { name: '<:emoji_47:1323358397684387900> | Top Moderadores', value: moderatorStats || 'Nenhuma ação registrada.', inline: false },
+          { name: '<:recente:1332355743592091780> | Ações Recentes', value: recentStats || 'Nenhuma ação registrada.', inline: false }
         )
         .setFooter({
           text: `${message.author.username}`,
