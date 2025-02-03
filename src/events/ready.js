77959d7959795ca597019e81f77b70096dc2beac
FuncZero <contato@funczero.xyz>
2025-02-03 15:45:58 +0000

    Atualização automática

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    console.log(`[INFO] Bot iniciado como ${client.user.tag}`);
+    console.log(`[SUCESSO] Bot iniciado como ${client.user.tag}`);
 
     client.user.setPresence({
       status: 'dnd',
