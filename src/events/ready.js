98bd9b58ebe415fb79c6cc31993f62f151cba657
FuncZero <contato@funczero.xyz>
2025-02-08 01:04:03 +0000

    Atualização automática

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,36 +2,14 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    try {
-      console.log(`[SUCESSO] Bot iniciado como ${client.user.tag}`);
-
-      
-      client.user.setPresence({
-        activities: [
-          {
-            name: '.help', 
-            type: 0, 
-          },
-        ],
-        status: 'dnd', 
-      });
-
-      console.log('[INFO] Presença configurada com sucesso: Jogando .help');
-    } catch (error) {
-      console.error('[ERROR] Erro ao configurar a presença:', error.message);
-    }
-
-    try {
-    
-      const guildCount = client.guilds.cache.size;
-      const userCount = client.guilds.cache.reduce(
-        (acc, guild) => acc + (guild.memberCount || 0),
-        0
-      );
-
-      console.log(`[INFO] Atualmente em ${guildCount} servidores, com um total de ${userCount} usuários.`);
-    } catch (error) {
-      console.error('[ERROR] Erro ao obter informações dos servidores ou usuários:', error.message);
-    }
+    client.user.setPresence({
+      status: 'dnd',
+      activities: [
+        {
+          name: '.help',
+          type: 0,
+        },
+      ],
+    });
   },
 };
