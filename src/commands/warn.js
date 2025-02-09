12b00a5dd811da4d73e4beabca99725103061b8f
FuncZero <contato@funczero.xyz>
2025-02-09 22:24:59 +0000

    Atualização automática

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder, PermissionsBitField } = require('discord.js');
-const db = require('../data/database');
+const db = require('../utils/database');
 
 module.exports = {
   name: 'warn',
