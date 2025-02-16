8920744a48eb29dc1be69c18e925469899b53d5a
FuncZero <contato@funczero.xyz>
2025-02-16 16:53:48 +0000

    Atualização automática

--- src/commands/privacy.js
+++ src/commands/privacy.js
@@ -6,26 +6,19 @@ module.exports = {
   usage: '${currentPrefix}privacy',
   permissions: 'Enviar Mensagens',
   execute: async (message) => {
-    
-    const lastUpdated = '<t:1739723760:f>';
+    const lastUpdated = '<t:1725585840:f>';
 
     const embed = new EmbedBuilder()
       .setColor(0xfe3838)
       .setTitle('Política de Privacidade')
       .setDescription(
         '**Ao utilizar o Punishment, você concorda com os seguintes termos:**\n\n' +
-          '**1. Processamento de Mensagens:**\n' +
           'O Punishment não armazena mensagens de texto dos usuários. Ele apenas processa conteúdos em tempo real para aplicar punições e enviar notificações de moderação.\n\n' +
-          '**2. Dados Coletados:**\n' +
           'Apenas dados essenciais do Discord são coletados, como ID do usuário, nome de perfil e informações sobre punições aplicadas.\n\n' +
-          '**3. Uso das Informações:**\n' +
           'Nenhuma informação pessoal é compartilhada ou utilizada para outros fins além da moderação.\n\n' +
-          '**4. Retenção e Remoção de Dados:**\n' +
           'Os dados coletados podem ser removidos permanentemente sem aviso prévio, caso necessário.\n\n' +
-          '**5. Consentimento:**\n' +
           'Ao continuar utilizando o Punishment, você concorda com essas regras.\n\n' +
-          '**6. Contato para Suporte:**\n' +
-          'Para dúvidas ou suporte, entre em contato pelo e-mail: contato@funczero.xyz.'
+          'Para dúvidas ou suporte, entre em contato pelo e-mail: [contato@funczero.xyz](mailto:contato@funczero.xyz).'
       )
       .addFields({
         name: '<:1000043158:1336324199202947144> Última Atualização',
