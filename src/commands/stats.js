c82d8771ef39f44ee354b74ab8996f40b613f8a7
FuncZero <contato@funczero.xyz>
2025-02-26 22:25:18 -0300

    Atualização automática

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -25,7 +25,7 @@ module.exports = {
       const uptime = formatUptime(process.uptime());
 
       const embed = new EmbedBuilder()
-        .setColor(0x36393F)
+        .setColor(0xFE3838)
         .setTitle(`${message.client.user.username} • Estatísticas`)
         .addFields(
           {
