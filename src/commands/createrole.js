aad71e41c4ab454b476c97c4d61a1426b2c78040
FuncZero <contato@funczero.xyz>
2025-03-07 18:55:23 -0300

    Atualização automática

--- src/commands/createrole.js
+++ src/commands/createrole.js
@@ -20,45 +20,40 @@ module.exports = {
     permissions: 'Gerenciar Cargos',
     async execute(message, args) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
-            const embedErroMinimo = new EmbedBuilder()
-      .setColor('#FF4C4C')
-      .setAuthor({
-          name: 'Você não possui permissão para usar este comando.',
-          iconURL: 'http://bit.ly/4aIyY9j'
-      });
-
-  return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({
+                embeds: [new EmbedBuilder()
+                    .setColor('#FF4C4C')
+                    .setAuthor({ name: 'Você não possui permissão para usar este comando.', iconURL: 'http://bit.ly/4aIyY9j' })],
+                allowedMentions: { repliedUser: false }
+            });
         }
 
         if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
-            const embedErroMinimo = new EmbedBuilder()
-      .setColor('#FF4C4C')
-      .setAuthor({
-          name: 'Não tenho permissão para criar cargos no servidor.',
-          iconURL: 'http://bit.ly/4aIyY9j'
-      });
-
-  return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({
+                embeds: [new EmbedBuilder()
+                    .setColor('#FF4C4C')
+                    .setAuthor({ name: 'Não tenho permissão para criar cargos no servidor.', iconURL: 'http://bit.ly/4aIyY9j' })],
+                allowedMentions: { repliedUser: false }
+            });
         }
 
         if (!args[0]) {
-            const embedErroMinimo = new EmbedBuilder()
-      .setColor('#FF4C4C')
-      .setAuthor({
-          name: 'Você precisa fornecer um nome para o cargo.',
-          iconURL: 'http://bit.ly/4aIyY9j'
-      });
-
-  return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({
+                embeds: [new EmbedBuilder()
+                    .setColor('#FF4C4C')
+                    .setAuthor({ name: 'Você precisa fornecer um nome para o cargo.', iconURL: 'http://bit.ly/4aIyY9j' })],
+                allowedMentions: { repliedUser: false }
+            });
         }
-        
+
         const roleName = args[0];
         const colorInput = args[1] ? args[1].toUpperCase() : '#FFFFFF';
         const roleColor = colorMapping[colorInput] || colorInput;
         const rolePermissions = args.slice(2).join(' ');
 
         try {
-            const resolvedPermissions = new PermissionsBitField();
+            let resolvedPermissions = new PermissionsBitField(0); // Inicializa sem permissões
+
             if (rolePermissions) {
                 rolePermissions
                     .toUpperCase()
@@ -66,7 +61,7 @@ module.exports = {
                     .map(perm => perm.trim())
                     .forEach(perm => {
                         if (PermissionsBitField.Flags[perm]) {
-                            resolvedPermissions.add(PermissionsBitField.Flags[perm]);
+                            resolvedPermissions = resolvedPermissions.add(PermissionsBitField.Flags[perm]);
                         }
                     });
             }
@@ -75,7 +70,7 @@ module.exports = {
                 name: roleName,
                 color: roleColor,
                 permissions: resolvedPermissions.bitfield,
-                reason: `Criado por ${message.author.displayName}`,
+                reason: `Criado por ${message.author.tag}`,
             });
 
             const embed = new EmbedBuilder()
@@ -92,14 +87,12 @@ module.exports = {
             message.channel.send({ embeds: [embed] });
         } catch (error) {
             console.error('Erro ao criar o cargo:', error);
-            const embedErroMinimo = new EmbedBuilder()
-      .setColor('#FF4C4C')
-      .setAuthor({
-          name: 'Não foi possível criar o cargo.',
-          iconURL: 'http://bit.ly/4aIyY9j'
-      });
-
-  return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({
+                embeds: [new EmbedBuilder()
+                    .setColor('#FF4C4C')
+                    .setAuthor({ name: 'Não foi possível criar o cargo.', iconURL: 'http://bit.ly/4aIyY9j' })],
+                allowedMentions: { repliedUser: false }
+            });
         }
     },
 };
