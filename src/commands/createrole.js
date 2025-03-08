464152f9758d5e0cb04480912d950a4244da0c40
FuncZero <contato@funczero.xyz>
2025-03-08 01:06:11 -0300

    Atualização automática

--- src/commands/createrole.js
+++ src/commands/createrole.js
@@ -20,60 +20,66 @@ module.exports = {
     permissions: 'Gerenciar Cargos',
     async execute(message, args) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
-            const embedErro = new EmbedBuilder()
-                .setColor('#FF4C4C')
-                .setAuthor({
-                    name: 'Você não possui permissão para usar este comando.',
-                    iconURL: 'http://bit.ly/4aIyY9j'
-                });
-
-            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
+            return message.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('#FF4C4C')
+                        .setAuthor({ 
+                            name: 'Você não possui permissão para usar este comando.', 
+                            iconURL: 'http://bit.ly/4aIyY9j' 
+                        })
+                ],
+                allowedMentions: { repliedUser: false }
+            });
         }
 
-        if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) {  
-            const embedErro = new EmbedBuilder()  
-                .setColor('#FF4C4C')  
-                .setAuthor({  
-                    name: 'Não tenho permissão para criar cargos no servidor.',  
-                    iconURL: 'http://bit.ly/4aIyY9j'  
-                });
-
-            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
+        if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
+            return message.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('#FF4C4C')
+                        .setAuthor({ 
+                            name: 'Não tenho permissão para criar cargos no servidor.', 
+                            iconURL: 'http://bit.ly/4aIyY9j' 
+                        })
+                ],
+                allowedMentions: { repliedUser: false }
+            });
         }
 
-        if (!args[0]) {  
-            const embedErro = new EmbedBuilder()  
-                .setColor('#FF4C4C')  
-                .setAuthor({  
-                    name: 'Você precisa fornecer um nome para o cargo.',  
-                    iconURL: 'http://bit.ly/4aIyY9j'  
-                });
-
-            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
+        if (!args[0]) {
+            return message.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('#FF4C4C')
+                        .setAuthor({ 
+                            name: 'Você precisa fornecer um nome para o cargo.', 
+                            iconURL: 'http://bit.ly/4aIyY9j' 
+                        })
+                ],
+                allowedMentions: { repliedUser: false }
+            });
         }
 
         const roleName = args[0];  
-        const colorInput = args[1] ? args[1].toUpperCase() : '#FFFFFF';  
+        const colorInput = args[1] ? args[1].toUpperCase() : 'WHITE';  
         const roleColor = colorMapping[colorInput] || colorInput;  
-        const rolePermissions = args.slice(2).join(' ');  
+        const permissionsInput = args.slice(2).join(' ');  
 
         try {  
             let resolvedPermissions = new PermissionsBitField();  
 
-            if (rolePermissions) {  
-                const permissionsArray = rolePermissions  
+            if (permissionsInput) {  
+                const permissionsArray = permissionsInput
                     .toUpperCase()
                     .replace(/,/g, ' ')
                     .split(/\s+/)
                     .map(perm => perm.trim());
 
-                console.log(`Permissões recebidas: ${permissionsArray}`);
-
-                resolvedPermissions = PermissionsBitField.resolve(
+                resolvedPermissions = new PermissionsBitField(
                     permissionsArray.filter(perm => PermissionsBitField.Flags[perm])
+                        .map(perm => PermissionsBitField.Flags[perm])
                 );
-
-                console.log(`Permissões finais (bitfield): ${resolvedPermissions}`);
             }  
 
             const newRole = await message.guild.roles.create({  
@@ -97,14 +103,17 @@ module.exports = {
             message.channel.send({ embeds: [embed] });  
         } catch (error) {  
             console.error('Erro ao criar o cargo:', error);  
-            const embedErro = new EmbedBuilder()  
-                .setColor('#FF4C4C')  
-                .setAuthor({  
-                    name: 'Não foi possível criar o cargo.',  
-                    iconURL: 'http://bit.ly/4aIyY9j'  
-                });
-
-            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
+            return message.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('#FF4C4C')
+                        .setAuthor({ 
+                            name: 'Não foi possível criar o cargo.', 
+                            iconURL: 'http://bit.ly/4aIyY9j' 
+                        })
+                ],
+                allowedMentions: { repliedUser: false }
+            });
         }
     },
 };
