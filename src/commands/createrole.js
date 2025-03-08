fa40dbc1a7bb5704a79e9b1fd926fd63da14ddb1
FuncZero <contato@funczero.xyz>
2025-03-08 00:48:05 -0300

    Atualização automática

--- src/commands/createrole.js
+++ src/commands/createrole.js
@@ -20,57 +20,53 @@ module.exports = {
     permissions: 'Gerenciar Cargos',
     async execute(message, args) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
-            const embedErroMinimo = new EmbedBuilder()
+            const embedErro = new EmbedBuilder()
                 .setColor('#FF4C4C')
                 .setAuthor({
                     name: 'Você não possui permissão para usar este comando.',
                     iconURL: 'http://bit.ly/4aIyY9j'
                 });
 
-            return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
         }
 
         if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) {  
-            const embedErroMinimo = new EmbedBuilder()  
+            const embedErro = new EmbedBuilder()  
                 .setColor('#FF4C4C')  
                 .setAuthor({  
                     name: 'Não tenho permissão para criar cargos no servidor.',  
                     iconURL: 'http://bit.ly/4aIyY9j'  
                 });
 
-            return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
         }
 
         if (!args[0]) {  
-            const embedErroMinimo = new EmbedBuilder()  
+            const embedErro = new EmbedBuilder()  
                 .setColor('#FF4C4C')  
                 .setAuthor({  
                     name: 'Você precisa fornecer um nome para o cargo.',  
                     iconURL: 'http://bit.ly/4aIyY9j'  
                 });
 
-            return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
         }
 
         const roleName = args[0];  
         const colorInput = args[1] ? args[1].toUpperCase() : '#FFFFFF';  
         const roleColor = colorMapping[colorInput] || colorInput;  
-        const rolePermissions = args.slice(2).join(' ');  
+        const rolePermissions = args.slice(2);  
 
         try {  
-            const resolvedPermissions = new PermissionsBitField();  
+            let resolvedPermissions = new PermissionsBitField();  
 
-            if (rolePermissions) {  
-                const permissionsArray = rolePermissions  
-                    .toUpperCase()
-                    .split(',')
-                    .map(perm => perm.trim());
-
-                permissionsArray.forEach(perm => {
-                    if (PermissionsBitField.Flags[perm]) {
-                        resolvedPermissions.add(PermissionsBitField.Flags[perm]);
+            if (rolePermissions.length > 0) {  
+                rolePermissions.forEach(perm => {
+                    const formattedPerm = perm.toUpperCase();
+                    if (PermissionsBitField.Flags[formattedPerm]) {
+                        resolvedPermissions.add(PermissionsBitField.Flags[formattedPerm]);
                     } else {
-                        console.log(`Permissão inválida: ${perm}`);
+                        console.log(`Permissão inválida: ${formattedPerm}`);
                     }
                 });  
             }  
@@ -78,8 +74,8 @@ module.exports = {
             const newRole = await message.guild.roles.create({  
                 name: roleName,  
                 color: roleColor,  
-                permissions: resolvedPermissions.bitfield,  
-                reason: `Criado por ${message.author.displayName}`,  
+                permissions: resolvedPermissions,  
+                reason: `Criado por ${message.author.tag}`,  
             });  
 
             const embed = new EmbedBuilder()  
@@ -96,14 +92,14 @@ module.exports = {
             message.channel.send({ embeds: [embed] });  
         } catch (error) {  
             console.error('Erro ao criar o cargo:', error);  
-            const embedErroMinimo = new EmbedBuilder()  
+            const embedErro = new EmbedBuilder()  
                 .setColor('#FF4C4C')  
                 .setAuthor({  
                     name: 'Não foi possível criar o cargo.',  
                     iconURL: 'http://bit.ly/4aIyY9j'  
                 });
 
-            return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
+            return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
         }
     },
 };
