4cdc4d87043759a2c36ca8aab2be42f47ffdab32
FuncZero <contato@funczero.xyz>
2025-03-16 18:56:27 -0300

    Atualização automática

--- src/slashCommands/help.js
+++ src/slashCommands/help.js
@@ -10,44 +10,47 @@ module.exports = {
         .setDescription('Nome do comando para obter informações detalhadas')
         .setRequired(false)
     ),
+
   async execute(interaction) {
     const commands = interaction.client.commands;
 
     if (!commands || commands.size === 0) {
-      const embedErroMinimo = new EmbedBuilder()
+      const embedErro = new EmbedBuilder()
         .setColor('#FF4C4C')
         .setAuthor({
           name: 'Parece que os comandos não foram carregados.',
-          iconURL: 'http://bit.ly/4aIyY9j'
+          iconURL: 'http://bit.ly/4aIyY9j',
         });
 
-      return interaction.reply({ embeds: [embedErroMinimo], ephemeral: true });
+      return interaction.reply({ embeds: [embedErro], ephemeral: true });
     }
 
     const commandName = interaction.options.getString('comando');
+    
     if (commandName) {
       const command = commands.get(commandName.toLowerCase());
 
       if (!command) {
-        const embedErroMinimo = new EmbedBuilder()
+        const embedErro = new EmbedBuilder()
           .setColor('#FF4C4C')
           .setAuthor({
             name: 'Não encontrei esse comando no sistema.',
-            iconURL: 'http://bit.ly/4aIyY9j'
+            iconURL: 'http://bit.ly/4aIyY9j',
           });
 
-        return interaction.reply({ embeds: [embedErroMinimo], ephemeral: true });
+        return interaction.reply({ embeds: [embedErro], ephemeral: true });
       }
 
-      const usage = command.usage || 'Não especificado.';
+      const usage = command.usage ? `\`/${command.data.name} ${command.usage}\`` : '`Não especificado.`';
+      const permissions = command.permissions ? `\`${command.permissions}\`` : '`Nenhuma`';
 
       const embed = new EmbedBuilder()
         .setColor(0x36393F)
         .setTitle(`<:1000042965:1336131844718202942> ${command.data.name}`)
         .setDescription(command.data.description || '`Nenhuma descrição disponível.`')
         .addFields(
-          { name: '<:1000043157:1336324220770062497> Uso', value: `\`${usage}\``, inline: false },
-          { name: '<:1000042960:1336120845881442365> Permissões Necessárias', value: `\`${command.permissions || 'Nenhuma'}\``, inline: false }
+          { name: '<:1000043157:1336324220770062497> Uso', value: usage, inline: false },
+          { name: '<:1000042960:1336120845881442365> Permissões Necessárias', value: permissions, inline: false }
         )
         .setFooter({
           text: 'Punishment',
@@ -61,12 +64,12 @@ module.exports = {
       .setColor(0x36393F)
       .setTitle('<:1000043167:1336329540502421576> Comandos Principais')
       .addFields(
-        { name: 'help', value: '`Exibe informações detalhadas sobre os comandos.`', inline: true },
-        { name: 'ping', value: '`Exibe os detalhes da conexão do bot.`', inline: true },
-        { name: 'privacy', value: '`Exibe a política de privacidade.`', inline: true },
-        { name: 'mod-stats', value: '`Exibe estatísticas da moderação no servidor.`', inline: true },
-        { name: 'stats', value: '`Exibe as estatísticas do bot.`', inline: true },
-        { name: 'undo', value: '`Desfaz o último comando executado.`', inline: true }
+        { name: '/help', value: '`Exibe informações detalhadas sobre os comandos.`', inline: true },
+        { name: '/ping', value: '`Exibe os detalhes da conexão do bot.`', inline: true },
+        { name: '/privacy', value: '`Exibe a política de privacidade.`', inline: true },
+        { name: '/mod-stats', value: '`Exibe estatísticas da moderação no servidor.`', inline: true },
+        { name: '/stats', value: '`Exibe as estatísticas do bot.`', inline: true },
+        { name: '/undo', value: '`Desfaz o último comando executado.`', inline: true }
       )
       .addFields(
         {
