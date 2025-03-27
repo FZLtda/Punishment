8abb502a1faa68aed293f360759cecb6dcb6ea22
FuncZero <contato@funczero.xyz>
2025-03-27 20:58:48 -0300

    Atualização automática

--- src/utils/presence.js
+++ src/utils/presence.js
@@ -1,22 +1,22 @@
 function setPresence(client) {
   client.once('ready', () => {
-  const statusMessages = [
-    '.help',
-    '.doar',
-    '.stats',
-    '.privacy'
-  ];
+    console.log('Bot está online!'); // Adicionando um log para garantir que o bot está pronto
 
-  let index = 0;
-  setInterval(() => {
-    client.user.setActivity(statusMessages[index], { type: 'PLAYING' }); // Altera o texto do status
-    console.log(`Status alterado para: ${statusMessages[index]}`);
-    
-    index = (index + 1) % statusMessages.length;
-  }, 5000);
-});
+    const statusMessages = [
+      '.help',
+      '.doar',
+      '.stats',
+      '.privacy'
+    ];
+
+    let index = 0;
+    setInterval(() => {
+      client.user.setActivity(statusMessages[index], { type: 'PLAYING' }); // Altera o texto do status
+      console.log(`Status alterado para: ${statusMessages[index]}`);
+      
+      index = (index + 1) % statusMessages.length;
+    }, 5000); // Muda o texto do status a cada 5000 milissegundos (5 segundos)
+  });
 }
 
 module.exports = { setPresence };
-
-
