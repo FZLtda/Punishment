4a255b67164d562e5588f92a700ccf7e68444b55
FuncZero <contato@funczero.xyz>
2025-03-27 21:04:22 -0300

    Atualização automática

--- src/utils/presence.js
+++ src/utils/presence.js
@@ -1,29 +1,26 @@
 function setPresence(client) {
   client.on('ready', () => {
-    console.log('Bot está online!');
     
     const statusMessages = [
-      '.help | .doar',
-      '.stats | .privacy',
-      '.ping | .uptime',
-      '.commands | .info'
+      '.help,
+      '.doar',
+      '.stats',
+      '.privacy'
     ];
 
     let index = 0;
 
     setInterval(() => {
       client.user.setPresence({
-        status: 'dnd', // Status "Não Perturbe"
+        status: 'dnd',
         activities: [{ 
-          name: statusMessages[index], // Alterando o texto
-          type: 0 // Tipo "Jogando" (PLAYING)
+          name: statusMessages[index],
+          type: 0
         }]
       });
-      console.log(`Status alterado para: ${statusMessages[index]}`);
       
-      // Atualiza o índice para o próximo status
       index = (index + 1) % statusMessages.length;
-    }, 5000); // Muda o texto do status a cada 5000 milissegundos (5 segundos)
+    }, 5000);
   });
 }
 
