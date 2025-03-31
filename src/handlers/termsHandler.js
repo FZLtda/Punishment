6df2e7c3bd4d061f1598d5523ff0481aceed893b
FuncZero <contato@funczero.xyz>
2025-03-31 21:24:36 +0000

    Atualização automática

--- src/handlers/termsHandler.js
+++ src/handlers/termsHandler.js
@@ -34,25 +34,4 @@ async function checkTerms(message) {
   return false;
 }
 
-async function handleTermsInteraction(interaction) {
-  const userId = interaction.user.id;
-
-  if (interaction.customId === 'accept_terms') {
-    
-    db.prepare('INSERT INTO terms (user_id) VALUES (?)').run(userId);
-
-    await interaction.update({
-      content: 'Você aceitou os Termos de Uso. Agora pode usar o bot!',
-      components: [],
-      embeds: [],
-    });
-  } else if (interaction.customId === 'decline_terms') {
-    await interaction.update({
-      content: 'Você recusou os Termos de Uso. Não poderá usar o bot.',
-      components: [],
-      embeds: [],
-    });
-  }
-}
-
-module.exports = { checkTerms, handleTermsInteraction };
+module.exports = { checkTerms };
