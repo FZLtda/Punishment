e0829d07c9b930a302e324b25438a9f0d99a4617
FuncZero <contato@funczero.xyz>
2025-04-04 22:08:56 +0000

    Atualização automática

--- src/commands/setprefix.js
+++ src/commands/setprefix.js
@@ -1,11 +1,12 @@
 const { EmbedBuilder } = require('discord.js');
-const { setPrefix } = require('../utils/prefixes');
+
 module.exports = {
   name: 'setprefix',
   description: 'Altera o prefixo do bot no servidor.',
   usage: '${currentPrefix}setprefix <prefixo>',
   permissions: 'Gerenciar Servidor',
   async execute(message, args, { setPrefix }) {
+    
     if (!message.member.permissions.has('ManageGuild')) {
       const embedErro = new EmbedBuilder()
         .setColor('#FF4C4C')
@@ -29,15 +30,30 @@ module.exports = {
     }
 
     const newPrefix = args[0];
-    setPrefix(message.guild.id, newPrefix);
 
-    const embedSucesso = new EmbedBuilder()
-      .setColor('#2ecc71')
-      .setAuthor({
-        name: `Prefixo atualizado com sucesso para: ${newPrefix}`,
-        iconURL: message.guild.iconURL({ dynamic: true }),
-      });
+    try {
 
-    return message.reply({ embeds: [embedSucesso], allowedMentions: { repliedUser: false } });
+      setPrefix(message.guild.id, newPrefix);
+
+      const embedSucesso = new EmbedBuilder()
+        .setColor('#2ecc71')
+        .setAuthor({
+          name: `Prefixo atualizado com sucesso para: ${newPrefix}`,
+          iconURL: message.guild.iconURL({ dynamic: true }),
+        });
+
+      return message.reply({ embeds: [embedSucesso], allowedMentions: { repliedUser: false } });
+    } catch (error) {
+      console.error('[ERROR] Falha ao atualizar o prefixo:', error);
+
+      const embedErro = new EmbedBuilder()
+        .setColor('#FF4C4C')
+        .setAuthor({
+          name: 'Ocorreu um erro ao tentar atualizar o prefixo.',
+          iconURL: 'https://bit.ly/43PItSI',
+        });
+
+      return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
+    }
   },
 };
