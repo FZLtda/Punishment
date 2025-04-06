8a93586309cfd9ffc1d1c367890f1a7a6b785ffc
FuncZero <contato@funczero.xyz>
2025-04-06 20:27:08 +0000

    Atualização automática

--- src/handlers/interactionHandler.js
+++
@@ -1,7 +0,0 @@
-const configMenu = require('../interactions/configMenu');
-const prefixModal = require('../interactions/prefixModal');
-
-module.exports = async (interaction, client) => {
-  await configMenu(interaction, client);
-  await prefixModal(interaction, client);
-};
