7bee6f5b2b033e4081a6b49b44dee68afde45a70
FuncZero <contato@funczero.xyz>
2025-04-10 21:30:23 +0000

    Atualização automática

--- src/commands/mute.js
+++ src/commands/mute.js
@@ -1,5 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
 const { logModerationAction } = require('../utils/moderationUtils');
+const { yellow } = require('../config/colors.json');
+const { icon_attention } = require('../config/emoji.json');
 
 module.exports = {
   name: 'mute',
@@ -84,10 +86,10 @@ module.exports = {
     } catch (error) {
       console.error(error);
       const embedErroMinimo = new EmbedBuilder()
-        .setColor('#FF4C4C')
+        .setColor(`${yellow}`)
         .setAuthor({
           name: 'Não foi possível silenciar o usuário devido a um erro.',
-          iconURL: 'https://bit.ly/43PItSI'
+          iconURL: `${icon_attention}`,          
         });
 
       return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
