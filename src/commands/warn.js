d2d7bfe770933076224b27990b189bb61ad24fa4
FuncZero <contato@funczero.xyz>
2025-06-14 04:57:51 -0300

    Atualização automática

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const db = require('../../data/database');
 const { yellow } = require('../../config/colors.json');
 const { icon_attention, attent } = require('../../config/emoji.json');
-const { LOG_CHANNEL } = require('../../config/settings.json');
+const { logChannelId } = require('../../config/settings.json');
 const { buildEmbed } = require('../../utils/embedUtils');
 const { applyPunishment } = require('../../utils/punishmentSystem');
 
