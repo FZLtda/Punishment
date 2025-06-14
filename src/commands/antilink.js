2071f2292a2729f4c2852d584d1b8594a7ecbea6
FuncZero <contato@funczero.xyz>
2025-06-14 06:27:32 -0300

    Atualização automática

--- src/commands/antilink.js
+++ src/commands/antilink.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const { icon_attention } = require('../config/emoji.json');
 const { yellow } = require('../config/colors.json');
 const fs = require('fs');
-const path = './data/antilink.json';
+const path = '../data/antilink.json';
 
 if (!fs.existsSync(path)) {
   fs.writeFileSync(path, JSON.stringify({}));
