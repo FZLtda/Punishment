e6b7af3e93608d5d84f4f0983c116d02fc151849
FuncZero <contato@funczero.xyz>
2025-06-14 04:49:14 -0300

    Atualização automática

---
+++ src/utils/embedUtils.js
@@ -0,0 +1,13 @@
+const { EmbedBuilder } = require('discord.js');
+
+function buildEmbed({ color, title, description, author, footer, timestamp = true }) {
+  const embed = new EmbedBuilder().setColor(color);
+  if (title) embed.setTitle(title);
+  if (description) embed.setDescription(description);
+  if (author) embed.setAuthor(author);
+  if (footer) embed.setFooter(footer);
+  if (timestamp) embed.setTimestamp();
+  return embed;
+}
+
+module.exports = { buildEmbed };
