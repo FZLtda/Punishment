f7951c124f8832823ee0dd784e2767e74fe9ee31
FuncZero <contato@funczero.xyz>
2025-06-14 16:02:30 -0300

    Atualização automática

--- src/commands/clearwarns.js
+++ src/commands/clearwarns.js
@@ -1,6 +1,8 @@
 const db = require('../data/database');
-const { yellow } = require('../config/colors.json');
+const { yellow, red } = require('../config/colors.json');
+const { icon_attention, attent } = require('../config/emoji.json');
 const { buildEmbed } = require('../utils/embedUtils');
+const { logModerationAction } = require('../utils/moderationUtils');
 
 module.exports = {
   name: 'clearwarns',
@@ -8,31 +10,72 @@ module.exports = {
   usage: '${currentPrefix}clearwarns <@usuário>',
   userPermissions: ['ManageMessages'],
   botPermissions: ['ManageMessages'],
+  deleteMessage: true,
 
   async execute(message) {
-    const user = message.mentions.members.first();
-    if (!user) {
-      return message.channel.send({
-        embeds: [buildEmbed({
-          color: yellow,
-          author: { name: 'Você precisa mencionar um usuário.', 
-          iconURL: message.author.displayAvatarURL() }
-        })],
-        allowedMentions: { repliedUser: false },
-      });
+    if (this.deleteMessage) {
+      message.delete().catch(() => {});
     }
 
-    db.prepare('DELETE FROM warnings WHERE user_id = ? AND guild_id = ?')
-      .run(user.id, message.guild.id);
+    const user = message.mentions.members.first();
 
-    return message.channel.send({
-      embeds: [buildEmbed({
+    if (!user) {
+      const embed = buildEmbed({
         color: yellow,
-        title: 'Avisos Removidos',
-        description: `Todos os avisos de ${user} foram removidos.`,
-        footer: { text: message.author.tag, iconURL: message.author.displayAvatarURL() }
-      })],
-      allowedMentions: { repliedUser: false },
-    });
-  }
+        author: {
+          name: 'Você precisa mencionar um usuário.',
+          iconURL: icon_attention,
+        },
+      });
+      return message.channel.send({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    }
+
+    const { count: totalWarnings } = db
+      .prepare('SELECT COUNT(*) AS count FROM warnings WHERE user_id = ? AND guild_id = ?')
+      .get(user.id, message.guild.id);
+
+    if (totalWarnings === 0) {
+      const embed = buildEmbed({
+        color: yellow,
+        author: {
+          name: 'Este usuário não possui avisos para remover.',
+          iconURL: icon_attention,
+        },
+      });
+      return message.channel.send({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    }
+
+    try {
+      db.prepare('DELETE FROM warnings WHERE user_id = ? AND guild_id = ?')
+        .run(user.id, message.guild.id);
+
+      logModerationAction(message.guild.id, message.author.id, 'ClearWarns', user.id, `Removeu ${totalWarnings} aviso(s)`);
+
+      const embed = buildEmbed({
+        color: red,
+        title: `${attent} Avisos Removidos`,
+        description: `Todos os **${totalWarnings}** aviso(s) de ${user} (\`${user.id}\`) foram removidos com sucesso.`,
+        thumbnail: user.user.displayAvatarURL({ dynamic: true }),
+        footer: {
+          text: message.author.tag,
+          iconURL: message.author.displayAvatarURL(),
+        },
+        timestamp: true,
+      });
+
+      return message.channel.send({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    } catch (error) {
+      console.error('Erro ao remover avisos:', error);
+
+      const embed = buildEmbed({
+        color: yellow,
+        author: {
+          name: 'Não foi possível remover os avisos devido a um erro interno.',
+          iconURL: icon_attention,
+        },
+      });
+
+      return message.channel.send({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    }
+  },
 };
