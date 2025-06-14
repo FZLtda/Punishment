c0eb34438577dce9543fac7186fc062f78e9d778
FuncZero <contato@funczero.xyz>
2025-06-14 12:36:34 -0300

    Atualização automática

--- src/commands/antilink.js
+++ src/commands/antilink.js
@@ -2,10 +2,12 @@ const { EmbedBuilder } = require('discord.js');
 const { icon_attention } = require('../config/emoji.json');
 const { yellow } = require('../config/colors.json');
 const fs = require('fs');
-const path = '../data/antilink.json';
+const path = require('path');
 
-if (!fs.existsSync(path)) {
-  fs.writeFileSync(path, JSON.stringify({}));
+const dataPath = path.resolve(__dirname, '../data/antilink.json');
+
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, JSON.stringify({}));
 }
 
 module.exports = {
@@ -15,28 +17,28 @@ module.exports = {
   userPermissions: ['ManageMessages'],
   botPermissions: ['ManageMessages'],
   deleteMessage: true,
-    
+
   async execute(message, args) {
-    
     const option = args[0]?.toLowerCase();
     const guildId = message.guild.id;
 
     if (!['on', 'off'].includes(option)) {
       const embedErro = new EmbedBuilder()
         .setColor(yellow)
-        .setAuthor({ 
-          name: 'Uso incorreto! Use `.antilink on` para ativar ou `.antilink off` para desativar.', 
-          iconURL: icon_attention });
+        .setAuthor({
+          name: 'Uso incorreto! Use `.antilink on` para ativar ou `.antilink off` para desativar.',
+          iconURL: icon_attention,
+        });
 
       return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
     }
 
     let settings;
     try {
-      settings = JSON.parse(fs.readFileSync(path, 'utf8'));
-    } catch (error) {
+      settings = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    } catch {
       settings = {};
-      fs.writeFileSync(path, JSON.stringify({}, null, 4));
+      fs.writeFileSync(dataPath, JSON.stringify({}, null, 4));
     }
 
     if (option === 'on') {
@@ -45,11 +47,13 @@ module.exports = {
       delete settings[guildId];
     }
 
-    fs.writeFileSync(path, JSON.stringify(settings, null, 4));
+    fs.writeFileSync(dataPath, JSON.stringify(settings, null, 4));
 
     const embed = new EmbedBuilder()
       .setColor(option === 'on' ? '#2ecc71' : '#fe3838')
-      .setTitle(option === 'on' ? '<:on:1232142357848260639> Antilink Ativado' : '<:emoji_51:1248416468819906721> Antilink Desativado')
+      .setTitle(option === 'on'
+        ? '<:on:1232142357848260639> Antilink Ativado'
+        : '<:emoji_51:1248416468819906721> Antilink Desativado')
       .setDescription(`O sistema de bloqueio de links foi ${option === 'on' ? 'ativado' : 'desativado'} neste servidor.`)
       .setFooter({ text: `${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
       .setTimestamp();
