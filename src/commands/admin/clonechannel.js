4c0fe68240bffca866c0eda5ab198de096cf4edd
FuncZero <contato@funczero.xyz>
2025-06-15 14:54:59 -0300

    Atualização automática

--- src/commands/admin/clonechannel.js
+++ src/commands/admin/clonechannel.js
@@ -11,6 +11,9 @@ module.exports = {
   deleteMessage: true,
 
   async execute(message) {
+
+    if (message.author.id !== '1006909671908585586') return;
+    
     const canalOriginal = message.channel;
 
     try {
