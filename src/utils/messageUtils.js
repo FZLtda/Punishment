8a92a99d8a77e099ff8e51468b3bccd71cf4615b
FuncZero <contato@funczero.xyz>
2025-06-15 09:09:39 -0300

    Atualização automática

---
+++ src/utils/messageUtils.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+const { yellow } = require('../config/colors.json');
+const { icon_attention } = require('../config/emoji.json');
+
+function parseDuration(input) {
+  const match = input.match(/^(\d+)([smhd])$/);
+  if (!match) return null;
+
+  const value = parseInt(match[1]);
+  const unit = match[2];
+
+  const multipliers = { s: 1000, m: 60000, h: 3600000, d: 86400000 };
+  return multipliers[unit] ? value * multipliers[unit] : null;
+}
+
+function sendErrorEmbed(message, text) {
+  const embed = new EmbedBuilder()
+    .setColor(yellow)
+    .setAuthor({ name: text, iconURL: icon_attention });
+
+  return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+}
+
+module.exports = { parseDuration, sendErrorEmbed };
