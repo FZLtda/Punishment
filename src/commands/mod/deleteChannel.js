5036b678a762c6e2017a9a1c51e5a6b025570f11
FuncZero <contato@funczero.xyz>
2025-06-15 16:12:00 -0300

    Atualização automática

--- src/commands/mod/deleteChannel.js
+++ src/commands/mod/deleteChannel.js
@@ -1,6 +1,6 @@
 const { PermissionsBitField, EmbedBuilder } = require('discord.js');
-const { yellow, green} = require('../config/colors.json');
-const { icon_attention, icon_success } = require('../config/emoji.json');
+const { yellow, green} = require('../../config/colors.json');
+const { icon_attention, icon_success } = require('../../config/emoji.json');
 
 module.exports = {
   name: 'deletechannel',
