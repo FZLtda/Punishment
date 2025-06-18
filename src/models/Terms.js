5b7fb3457c37eea0b0c286f826d97b99527d343c
FuncZero <contato@funczero.xyz>
2025-06-17 23:02:53 -0300

    Atualização automática

--- src/models/Terms.js
+++ src/models/Terms.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const termsSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  acceptedAt: { type: Date, default: Date.now },
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  acceptedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('Terms', termsSchema);
