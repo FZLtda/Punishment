d06186fd2575043ecf40f8f2ee8da3c05e336c7d
FuncZero <contato@funczero.xyz>
2025-06-17 21:51:36 -0300

    Atualização automática

--- src/models/test.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const testSchema = new Schema({
-  userId: String,
-  content: String,
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = model('Test', testSchema);
