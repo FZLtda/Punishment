4894c602543d09108c45bd907833313af9b9a15c
FuncZero <contato@funczero.xyz>
2025-06-18 01:14:37 -0300

    Atualização automática

--- src/utils/giveawayUtils.js
+++ src/utils/giveawayUtils.js
@@ -46,7 +46,7 @@ function gerarMensagemVencedores(winners, prize) {
     return `${attent} Nenhum vencedor foi escolhido porque ninguém participou.`;
   }
 
-  const mencoes = winners.map(id => `{id}`).join(', ');
+  const mencoes = winners.map(id => `<@{id}>`).join(', ');
   return winners.length === 1
     ? `🎉 Parabéns ${mencoes}! Você ganhou o **${prize}**!`
     : `🎉 Parabéns ${mencoes}! Vocês ganharam o **${prize}**!`;
