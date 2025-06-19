aa7b01da2ca6f228bc21c3fac0fe0488b3295ea9
FuncZero <contato@funczero.xyz>
2025-06-18 21:52:18 -0300

    Atualização automática

--- src/handlers/interactions/commands/default.js
+++
@@ -1,5 +0,0 @@
-const { handleSlashCommand } = require('../../slashCommandHandler');
-
-module.exports = async function handleCommand(interaction, client) {
-  return await handleSlashCommand(interaction, client);
-};
