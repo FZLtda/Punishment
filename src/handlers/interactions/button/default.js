a1b8d58d4fe328ae7459a1752fc2750bc1f42f37
FuncZero <contato@funczero.xyz>
2025-06-19 22:10:03 -0300

    Delete - FZ

--- src/handlers/interactions/button/default.js
+++
@@ -1,5 +0,0 @@
-const { handleButtonInteraction } = require('../../buttonHandler');
-
-module.exports = async function handleButton(interaction, client) {
-  return await handleButtonInteraction(interaction, client);
-};
