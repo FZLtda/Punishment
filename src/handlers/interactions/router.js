2cdee202835995c3548b12c7263f9beb853f7f2e
FuncZero <contato@funczero.xyz>
2025-06-19 21:35:07 -0300

    Atualização automática

--- src/handlers/interactions/router.js
+++ src/handlers/interactions/router.js
@@ -1,16 +1,12 @@
-const buttonHandlers = require('./buttons/default');
-const commandHandlers = require('./commands/default');
-
+const { handleButtonInteraction } = require('./buttons/default');
 
 module.exports = async function routeInteraction(interaction, client, type) {
   switch (type) {
     case 'button':
-      return await buttonHandlers(interaction, client);
+      return await handleButtonInteraction(interaction, client);
 
     case 'command':
       return await commandHandlers(interaction, client);
-
-
 
     default:
       throw new Error(`Tipo de interação não roteável: ${type}`);
