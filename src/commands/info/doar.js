4e50c0503896e76391c49513a3ec7fd3558ead08
FuncZero <contato@funczero.xyz>
2025-06-21 17:50:40 -0300

    chore(alias): migra de Babel para module-alias com suporte a @paths

--- src/commands/info/doar.js
+++ src/commands/info/doar.js
@@ -1,6 +1,9 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { yellow, red } = require('../../config/colors.json');
-const { icon_attention } = require('../../config/emoji.json');
+const { 
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder, 
+  ButtonStyle } = require('discord.js');
+const { colors, emojis } = require('@config');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = {
@@ -14,10 +17,10 @@ module.exports = {
   async execute(message, args) {
     if (!args[0] || isNaN(args[0])) {
       const embedErro = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Informe um valor válido! Exemplo: .doar 10',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
@@ -26,10 +29,10 @@ module.exports = {
     const valor = parseFloat(args[0]) * 100;
     if (valor < 100) {
       const embedErroMinimo = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'O valor mínimo para doação é R$1,00.',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       return message.reply({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
@@ -61,7 +64,7 @@ module.exports = {
         );
 
       const embed = new EmbedBuilder()
-        .setColor(red)
+        .setColor(colors.red)
         .setTitle('Doação Iniciada')
         .setDescription(
           `${message.author}, você está doando **R$${(valor / 100).toFixed(2)}** para o **Punishment**.`
@@ -79,10 +82,10 @@ module.exports = {
       console.error('Erro ao criar a sessão de pagamento:', error);
 
       const embedErro = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Algo deu errado. Tente novamente mais tarde.',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       await message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
