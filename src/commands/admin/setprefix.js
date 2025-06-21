4213c88c84a59a88755d6e8c8b3feb1cf9c132a5
FuncZero <contato@funczero.xyz>
2025-06-21 17:17:09 -0300

    chore(alias): migra de Babel para module-alias com suporte a @paths

--- src/commands/admin/setprefix.js
+++ src/commands/admin/setprefix.js
@@ -1,6 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
-const { yellow, green } = require('../../config/colors.json');
-const { icon_attention, check } = require('../../config/emoji.json');
+const { colors, emojis } = require('@config');
 
 module.exports = {
   name: 'setprefix',
@@ -16,10 +15,10 @@ module.exports = {
 
     if (!newPrefix) {
       const embedErro = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Você precisa informar o novo prefixo. Exemplo: .setprefix !',
-          iconURL: icon_attention,
+          iconURL: emojis.icon_attention,
         });
 
       return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
@@ -27,10 +26,10 @@ module.exports = {
 
     if (newPrefix.length > 5) {
       const embedErro = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'O prefixo não pode ter mais de 5 caracteres.',
-          iconURL: icon_attention,
+          iconURL: emojis.icon_attention,
         });
 
       return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
@@ -41,8 +40,8 @@ module.exports = {
       setPrefix(message.guild.id, newPrefix);
 
       const embedSucesso = new EmbedBuilder()
-        .setColor(green)
-        .setTitle(`${check} Atualização de Prefixo`)
+        .setColor(colors.green)
+        .setTitle(`${emojis.check} Atualização de Prefixo`)
         .setDescription(`Prefixo atualizado para: ${newPrefix}`);
 
       return message.reply({ embeds: [embedSucesso], allowedMentions: { repliedUser: false } });
@@ -51,10 +50,10 @@ module.exports = {
       console.error('[ERRO] Falha ao atualizar o prefixo:', error);
 
       const embedErro = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Erro ao atualizar o prefixo. Tente novamente mais tarde.',
-          iconURL: icon_attention,
+          iconURL: emojis.icon_attention,
         });
 
       return message.reply({ embeds: [embedErro], allowedMentions: { repliedUser: false } });
