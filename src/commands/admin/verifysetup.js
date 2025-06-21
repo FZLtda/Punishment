01b33f9c284ffbb45e8242d25b679a6b1f7c89c2
FuncZero <contato@funczero.xyz>
2025-06-21 17:15:07 -0300

    chore(alias): migra de Babel para module-alias com suporte a @paths

--- src/commands/admin/verifysetup.js
+++ src/commands/admin/verifysetup.js
@@ -4,8 +4,7 @@ const {
   ButtonStyle,
   EmbedBuilder,
 } = require('discord.js');
-const { green } = require('../../config/colors.json');
-const { check } = require('../../config/emoji.json');
+const { colors, emojis } = require('@config');
 
 module.exports = {
   name: 'verifysetup',
@@ -20,7 +19,7 @@ module.exports = {
     if (message.author.id !== '1006909671908585586') return;
     
     const embed = new EmbedBuilder()
-      .setColor(green)
+      .setColor(colors.green)
       .setTitle('Verificação Necessária')
       .setDescription('Clique no botão abaixo para se verificar e ter acesso completo ao servidor.')
       .setFooter({
@@ -33,7 +32,7 @@ module.exports = {
         .setCustomId('verify_user')
         .setLabel('Verificar')
         .setStyle(ButtonStyle.Success)
-        .setEmoji(check)
+        .setEmoji(emojis.check)
     );
 
     await message.channel.send({ embeds: [embed], components: [row] });
