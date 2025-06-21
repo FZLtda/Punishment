84e603e6e69a0e815b8e98423ae4242cadfad88b
FuncZero <contato@funczero.xyz>
2025-06-21 13:33:59 -0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,6 +1,6 @@
 const logger = require('@utils/logger');
 const { attent } = require('@config');
-const { checkTerms } = require('@handleEvent/termsButtons');
+const { checkTerms } = require('@handleButton/termsButtons');
 const router = require('@interactions/router');
 
 module.exports = {
