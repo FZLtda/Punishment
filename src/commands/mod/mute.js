847b9495a57a6e01836802cb0c9acb2ed4547cf9
FuncZero <contato@funczero.xyz>
2025-06-21 19:01:02 -0300

    chore(alias): migra de Babel para module-alias com suporte a @paths

--- src/commands/mod/mute.js
+++ src/commands/mod/mute.js
@@ -1,7 +1,6 @@
 const { EmbedBuilder } = require('discord.js');
-const { logModerationAction } = require('../../utils/moderationUtils');
-const { yellow, red } = require('../../config/colors.json');
-const { icon_attention } = require('../../config/emoji.json');
+const { logModerationAction } = require('@utils/moderationUtils');
+const { colors, emojis } = require('@config');
 
 module.exports = {
   name: 'mute',
@@ -19,10 +18,10 @@ module.exports = {
 
     if (!membro) {
       const embedErroMinimo = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Mencione um usuário para executar esta ação.',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       return message.channel.send({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
@@ -30,10 +29,10 @@ module.exports = {
 
     if (!tempo) {
       const embedErroMinimo = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Defina um tempo de duração para prosseguir (ex: 1m, 1h, 1d).',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       return message.channel.send({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
@@ -42,10 +41,10 @@ module.exports = {
     const duracao = convertToMilliseconds(tempo);
     if (!duracao) {
       const embedErroMinimo = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Duração inválida. Forneça um valor válido (ex: 1m, 1h, 1d).',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       return message.channel.send({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
@@ -53,10 +52,10 @@ module.exports = {
 
     if (!membro.moderatable) {
       const embedErroMinimo = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Este usuário não pode ser silenciado devido às suas permissões.',
-          iconURL: icon_attention
+          iconURL: emojis.icon_attention
         });
 
       return message.channel.send({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
@@ -69,7 +68,7 @@ module.exports = {
 
       const embed = new EmbedBuilder()
         .setTitle('<:Mutado:1355700779859574954> Punição aplicada')
-        .setColor(red)
+        .setColor(colors.red)
         .setDescription(`${membro} (\`${membro.id}\`) foi mutado(a)!`)
         .addFields(
           { name: 'Duração', value: `\`${tempo}\``, inline: true },
@@ -86,10 +85,10 @@ module.exports = {
     } catch (error) {
       console.error(error);
       const embedErroMinimo = new EmbedBuilder()
-        .setColor(yellow)
+        .setColor(colors.yellow)
         .setAuthor({
           name: 'Não foi possível silenciar o usuário devido a um erro.',
-          iconURL: icon_attention,          
+          iconURL: emojis.icon_attention,          
         });
 
       return message.channel.send({ embeds: [embedErroMinimo], allowedMentions: { repliedUser: false } });
