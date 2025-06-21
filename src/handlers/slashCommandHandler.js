31f5b6aba75468023dbe7bbb6066b80b7ba7b43f
FuncZero <contato@funczero.xyz>
2025-06-20 22:45:22 -0300

    Atualização automática

--- src/handlers/slashCommandHandler.js
+++ src/handlers/slashCommandHandler.js
@@ -1,11 +1,11 @@
-const logger = require('../utils/logger');
-const { error, attent } = require('../config/emoji.json');
+const logger = require('@utils/logger');
+const { emoji } = require('@config');
 
 async function handleSlashCommand(interaction, client) {
   const command = client.slashCommands.get(interaction.commandName);
   if (!command) {
     return interaction.reply({
-      content: `${attent} Este comando ainda não foi registrado.`,
+      content: `${emoji.attent} Este comando ainda não foi registrado.`,
       ephemeral: true,
     });
   }
@@ -15,7 +15,7 @@ async function handleSlashCommand(interaction, client) {
   } catch {
     logger.error(`ERRO: Erro ao executar Slash Command "${interaction.commandName}": ${error.message}`, { stack: error.stack });
     await interaction.reply({
-      content: `${attent} Não foi possível processar o comando.`,
+      content: `${emoji.attent} Não foi possível processar o comando.`,
       ephemeral: true,
     });
   }
