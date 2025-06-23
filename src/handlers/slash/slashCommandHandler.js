40507356acc866eb62645f18f32be6aa2cfe7b0d
FuncZero <contato@funczero.xyz>
2025-06-23 20:21:12 -0300

    chore(alias): migra de Babel para module-alias com suporte a @paths

--- src/handlers/slash/slashCommandHandler.js
+++ src/handlers/slash/slashCommandHandler.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const logger = require('@utils/logger');
 const { emojis } = require('@config');
 
@@ -5,17 +7,17 @@ async function handleSlashCommand(interaction, client) {
   const command = client.slashCommands.get(interaction.commandName);
   if (!command) {
     return interaction.reply({
-      content: `${emoji.attent} Este comando ainda não foi registrado.`,
+      content: `${emojis.attent} Este comando ainda não foi registrado.`,
       ephemeral: true,
     });
   }
 
   try {
     await command.execute(interaction);
-  } catch {
+  } catch (error) {
     logger.error(`ERRO: Erro ao executar Slash Command "${interaction.commandName}": ${error.message}`, { stack: error.stack });
     await interaction.reply({
-      content: `${emoji.attent} Não foi possível processar o comando.`,
+      content: `${emojis.attent} Não foi possível processar o comando.`,
       ephemeral: true,
     });
   }
