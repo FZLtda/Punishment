f529896105263852a9a81588c451ff5b3877a710
FuncZero <contato@funczero.xyz>
2025-06-30 19:43:26 -0300

    chore: adiciona emojis padrão do bot

---
+++ src/config/emojis.js
@@ -0,0 +1,6 @@
+module.exports = {
+  success: '<:sucesso:1358918549846098201>',
+  error: '<:error:1358918322770677971>',
+  attent: '<:attention:1358918618808717704>',
+  attention: 'https://bit.ly/3Ypxwnl'
+};
