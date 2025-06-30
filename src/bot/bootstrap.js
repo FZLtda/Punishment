823b5b0c4ad8de9ae734a4a604e69e1356a61699
FuncZero <contato@funczero.xyz>
2025-06-30 18:24:55 -0300

    feat: adiciona função bootstrap para inicialização do bot

---
+++ src/bot/bootstrap.js
@@ -0,0 +1,32 @@
+const Logger = require('@logger/index');
+const client = require('@bot/client');
+const { validateEnvironment } = require('@bot/environment');
+const { loadCommands } = require('@handlers/commands/loader');
+const { loadEvents } = require('@handlers/events/loader');
+const { loadSlashCommands } = require('@handlers/slash/loader');
+const { loadButtonInteractions } = require('@handlers/interactions/buttons/loader');
+
+module.exports = async function bootstrap() {
+  try {
+    Logger.info('Validando variáveis de ambiente...');
+    validateEnvironment();
+
+    Logger.info('Carregando comandos...');
+    await loadCommands(client);
+
+    Logger.info('Carregando eventos...');
+    await loadEvents(client);
+
+    Logger.info('Carregando slash commands...');
+    await loadSlashCommands(client);
+
+    Logger.info('Carregando interações de botão...');
+    await loadButtonInteractions(client);
+
+    Logger.info('Conectando ao Discord...');
+    await client.login(process.env.TOKEN);
+  } catch (err) {
+    Logger.error(`Falha ao inicializar o bot: ${err.message}`);
+    process.exit(1);
+  }
+};
