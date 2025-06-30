7756d4441960aba259ac8dc376559a03ec2ec210
FuncZero <contato@funczero.xyz>
2025-06-30 17:25:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('module-alias/register');
 const os = require('os');
 const logger = require('@utils/logger');
 const connectDatabase = require('@utils/database');
-const startBot = require('@src');
+const startBot = require('@src/bot');
 const { BOT_NAME } = require('@config');
 
 (async () => {
