bea0ede71fbbcfc7cf6dd139eaa7daf88a404afe
FuncZero <contato@funczero.xyz>
2025-07-02 04:01:14 -0300

    chore(config): adiciona novos emojis personalizados ao módulo de emojis

--- src/config/emojis.js
+++ src/config/emojis.js
@@ -2,5 +2,11 @@ module.exports = {
   success: '<:check:1357137501520662549>',
   error: '<:error:1358918322770677971>',
   attent: '<:attention:1358918618808717704>',
+  mute: '<:Mutado:1355700779859574954>',
+  unmute: '<:Desmutado:1355700639681740880>',
+  ban: '<:Banido:1355700878056751244>',
+  unban: '<:Desbanido:1355718942076965016>',
+  kick: '<:Expulso:1355700922197606573>',
+  
   attention: 'https://bit.ly/3Ypxwnl'
 };
