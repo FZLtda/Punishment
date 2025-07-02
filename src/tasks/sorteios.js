ab616173604a59a654034b9dce4740bd96ede162
FuncZero <contato@funczero.xyz>
2025-07-02 19:00:01 -0300

    chore(tasks/sorteios): adiciona log de cada sorteio finalizado via logger - Facilita auditoria e rastreamento de sorteios encerrados com sucesso

--- src/tasks/sorteios.js
+++ src/tasks/sorteios.js
@@ -1,16 +1,25 @@
+'use strict';
+
 const Giveaway = require('@models/Giveaway');
 const { finalizarSorteio } = require('@utils/sorteios');
+const Logger = require('@logger'); 
 
 module.exports = (client) => {
   setInterval(async () => {
     const agora = new Date();
-    const pendentes = await Giveaway.find({
-      endsAt: { $lte: agora },
-      status: 'ativo'
-    });
 
-    for (const sorteio of pendentes) {
-      await finalizarSorteio(sorteio, client);
+    try {
+      const pendentes = await Giveaway.find({
+        endsAt: { $lte: agora },
+        status: 'ativo'
+      });
+
+      for (const sorteio of pendentes) {
+        await finalizarSorteio(sorteio, client);
+        Logger.info(`[SORTEIO] Sorteio finalizado: "${sorteio.prize}" • ${sorteio.guildId}`);
+      }
+    } catch (err) {
+      Logger.error(`[SORTEIO] Erro ao finalizar sorteios: ${err.stack || err.message}`);
     }
   }, 60 * 1000);
 };
