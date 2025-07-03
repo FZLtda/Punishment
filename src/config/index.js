b0393d9d4c11f42578605348223a31792cecefcd
FuncZero <contato@funczero.xyz>
2025-07-03 06:58:37 -0300

    chore: exporta módulos de cores, emojis, cargos e canais centralizados

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 const colors = require('./colors');
 const emojis = require('./emojis');
 const roles = require('./roles');
+const channels = require('./channels');
 
 module.exports = {
   colors,
   roles,
-  emojis
+  emojis,
+  channels
 };
