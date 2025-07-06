106400f72d6ea871b9405d510e492d2e9bf632ef
FuncZero <contato@funczero.xyz>
2025-07-06 09:39:05 -0300

    fix: correções na embed principal

--- src/commands/admin/regras.js
+++ src/commands/admin/regras.js
@@ -35,17 +35,23 @@ module.exports = {
       .setTitle('Regras do Servidor')
       .setColor(colors.green)
       .setDescription([
-        `**1. Respeito** <:seta2:1325132415542231140> Trate todos com educação. Ofensas, discriminação e discussões desnecessárias não serão toleradas.`,
+        `**1. Respeito** 
+        <:seta2:1325132415542231140> Trate todos com educação. Ofensas, discriminação e discussões desnecessárias não serão toleradas.`,
         ``,
-        `**2. Conteúdo Apropriado** <:seta2:1325132415542231140> Não publique spam, flood, links maliciosos ou conteúdos inadequados.`,
+        `**2. Conteúdo Apropriado** 
+        <:seta2:1325132415542231140> Não publique spam, flood, links maliciosos ou conteúdos inadequados.`,
         ``,
-        `**3. Suporte** <:seta2:1325132415542231140> Use o canal de suporte apenas para dúvidas ou problemas relacionados ao **Punishment**.`,
+        `**3. Suporte** 
+        <:seta2:1325132415542231140> Use o canal de suporte apenas para dúvidas ou problemas relacionados ao **Punishment**.`,
         ``,
-        `**4. Divulgação** <:seta2:1325132415542231140> É proibido divulgar qualquer conteúdo sem autorização da equipe.`,
+        `**4. Divulgação** 
+        <:seta2:1325132415542231140> É proibido divulgar qualquer conteúdo sem autorização da equipe.`,
         ``,
-        `**5. Segurança** <:seta2:1325132415542231140> Nunca compartilhe dados pessoais. A equipe nunca pedirá suas informações sensíveis.`,
+        `**5. Segurança** 
+        <:seta2:1325132415542231140> Nunca compartilhe dados pessoais. A equipe nunca pedirá suas informações sensíveis.`,
         ``,
-        `**6. Punições** <:seta2:1325132415542231140> O descumprimento das regras resultará em punições. A moderação tem a palavra final.`,
+        `**6. Punições** 
+        <:seta2:1325132415542231140> O descumprimento das regras resultará em punições. A moderação tem a palavra final.`,
         ``,
         `> **__Clique no botão abaixo para aceitar as regras.__**`,
       ].join('\n'))
