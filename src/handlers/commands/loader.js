2da38581280e1cc91e59496c4b96f13f8268674a
FuncZero <contato@funczero.xyz>
2025-07-09 08:45:35 -0300

    fix: corrigir caminho do logger

--- src/handlers/commands/loader.js
+++ src/handlers/commands/loader.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const Logger = require('@logger/index');
+const Logger = require('@logger');
 
 async function loadCommands(client) {
   const commandsPath = path.join(__dirname, '../../../src/commands');
