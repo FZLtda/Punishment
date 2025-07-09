f0ca856e95aef0fb04c1730c211fc3e282c8a97f
FuncZero <contato@funczero.xyz>
2025-07-09 08:45:06 -0300

    fix: corrigir caminho do logger

--- src/handlers/events/loader.js
+++ src/handlers/events/loader.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const Logger = require('@logger/index');
+const Logger = require('@logger');
 
 async function loadEvents(client) {
   const eventsPath = path.join(__dirname, '../../../src/events');
