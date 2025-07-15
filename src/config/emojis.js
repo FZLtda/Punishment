6ffd157264b7c602dc37e349770f38516df21b17
FuncZero <contato@funczero.xyz>
2025-07-15 20:07:54 -0300

    chore(config): atualiza emoji em emojis.js para refletir nova identidade visual

--- src/config/emojis.js
+++ src/config/emojis.js
@@ -7,7 +7,7 @@ module.exports = {
   // Emojis de status e feedback
   attentionEmoji: '<:1000055967:1358918618808717704>',
   successEmoji:   '<:1000075728:1394121393947545720>',
-  errorEmoji:     '<:1000075729:1394121565645443204>',
+  errorEmoji:     '<:1000076248:1394817252896931870>',
   infoEmoji:      '<:1000046547:1340465282568945675>',
 
   // Emojis de comandos de moderação
