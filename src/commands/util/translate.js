4cb9710c9bb84d1e7694a794f11ca9dd6680520c
FuncZero <contato@funczero.xyz>
2025-07-16 20:56:53 -0300

    add emoji no embed

--- src/commands/util/translate.js
+++ src/commands/util/translate.js
@@ -36,7 +36,7 @@ module.exports = {
     }
 
     const embed = new EmbedBuilder()
-      .setTitle(`${emojis.translate || '🌐'} Tradução`)
+      .setTitle('<:1000046512:1340411258427408444> Tradução')
       .setColor(colors.red)
       .addFields(
         { name: 'Original', value: replied.content.slice(0, 1024) },
