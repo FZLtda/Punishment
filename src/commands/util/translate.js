88d4645384568f7fe014bdf1afb9ba4f23b56235
FuncZero <contato@funczero.xyz>
2025-07-16 21:06:17 -0300

    feat(cmd/translate): adicionar emoji ao comando translate

--- src/commands/util/translate.js
+++ src/commands/util/translate.js
@@ -36,7 +36,7 @@ module.exports = {
     }
 
     const embed = new EmbedBuilder()
-      .setTitle('<:1000046512:1340411258427408444> Tradução')
+      .setTitle(`${emonis.trad} Tradução`)
       .setColor(colors.red)
       .addFields(
         { name: 'Original', value: replied.content.slice(0, 1024) },
