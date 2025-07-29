40e4a740d246676a351b4a909aba59c2d46ab086
FuncZero <contato@funczero.xyz>
2025-07-29 04:25:30 -0300

    refactor(config): organiza e padroniza index.js com use strict

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const langFlags = require('./langFlags');
-const channels = require('./channels');
-const colors = require('./colors');
-const emojis = require('./emojis');
-const roles = require('./roles');
-const bot = require('./bot');
+const channels  = require('./channels');
+const colors    = require('./colors');
+const emojis    = require('./emojis');
+const roles     = require('./roles');
+const bot       = require('./bot');
 
 module.exports = {
   langFlags,
@@ -12,5 +14,4 @@ module.exports = {
   emojis,
   roles,
   bot
-  
 };
