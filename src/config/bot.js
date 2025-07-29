203067d9be4ab9b75b62a7aa0ba8ff9e9c3ac58b
FuncZero <contato@funczero.xyz>
2025-07-29 04:19:12 -0300

    refactor(config): reorganiza e limpa o arquivo de informações do bot

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,7 +1,12 @@
+'use strict';
+
 module.exports = {
   name: 'Punishment',
+  description: 'Punishment é um bot de moderação avançado para Discord, projetado para manter servidores organizados e seguros.',
   version: '1.0.0',
   color: '#FE3838',
   logo: 'http://bit.ly/4kzlmjU',
-  owner: '1006909671908585586'
+  ownerTag: 'funczero',
+  ownerId: '1006909671908585586',
+  createdAt: '2023-09-23'
 };
