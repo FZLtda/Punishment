d7a8cdadbf5148c6d838816e1fb32899c5a1e07b
FuncZero <contato@funczero.xyz>
2025-07-29 04:06:55 -0300

    chore(colors): adiciona e organiza novas cores padrão para embeds

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,5 +1,14 @@
 module.exports = {
-  red: 0xFE3838,
+  red:    0xFC544C,
   yellow: 0xFBD35B,
-  green: 0x2ECC71
+  green:  0x2ECC71,
+  blue:   0x3498DB,
+  orange: 0xE67E22,
+  purple: 0x9B59B6,
+  pink:   0xFF69B4,
+  cyan:   0x1ABC9C,
+  white:  0xECF0F1,
+  gray:   0x95A5A6,
+  dark:   0x2C3E50,
+  black:  0x000000
 };
