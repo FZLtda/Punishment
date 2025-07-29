b890e34411b08853367abd0c07c3ab9cd9efd266
FuncZero <contato@funczero.xyz>
2025-07-29 03:41:37 -0300

    refactor: aplicar sendError no verificador de permissões do bot

--- src/utils/checkBotPermissions.js
+++ src/utils/checkBotPermissions.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const { EmbedBuilder } = require('discord.js');
-const { colors, emojis } = require('@config');
+const { sendError } = require('@utils/embedError');
 
 /**
  * Verifica se o bot possui as permissões necessárias.
@@ -10,23 +9,13 @@ const { colors, emojis } = require('@config');
  * @param {string[]} requiredPermissions
  * @returns {Promise<boolean>}
  */
+
 module.exports = async function checkBotPermissions(botMember, message, requiredPermissions = []) {
   if (!botMember?.permissions) return false;
 
   const missing = requiredPermissions.filter(p => !botMember.permissions.has(p));
   if (missing.length === 0) return true;
 
-  const embed = new EmbedBuilder()
-    .setColor(colors.red)
-    .setAuthor({
-      name: 'Eu não tenho permissão suficiente para executar este comando.',
-      iconURL: emojis.errorIcon
-    });
-
-  await message.channel.send({
-    embeds: [embed],
-    allowedMentions: { repliedUser: false }
-  });
-
+  await sendError(message, 'Eu não tenho permissão suficiente para executar este comando.');
   return false;
 };
