92c20a8464cf007626b415069c7573340913c088
FuncZero <contato@funczero.xyz>
2025-08-01 20:13:35 -0300

    fix: remover envio de mensagem DM ao usuário ao atribuir cargo de doador

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const { Events, EmbedBuilder } = require('discord.js');
+const { Events } = require('discord.js');
 const logger = require('@logger');
-const { colors } = require('@config');
 const PendingDonor = require('@models/PendingDonor');
 
 const CARGO_DOADOR_ID = process.env.CARGO_DOADOR_ID;
@@ -25,16 +24,6 @@ module.exports = {
       await PendingDonor.deleteOne({ userId: member.id });
 
       logger.info(`[Doador] Cargo atribuído a ${member.user.tag} (${member.id})`);
-
-      const embed = new EmbedBuilder()
-        .setTitle('🎉 Obrigado por apoiar o Punishment!')
-        .setDescription('Você recebeu automaticamente o cargo de doador ao entrar no servidor.\nMuito obrigado pelo seu apoio!')
-        .setColor(colors.green)
-        .setFooter({ text: 'Sistema de Recompensas' });
-
-      await member.send({ embeds: [embed] }).catch(() => {
-        logger.warn(`[Doador] Não foi possível enviar DM para ${member.user.tag}.`);
-      });
 
     } catch (err) {
       logger.error(`[Doador] Erro ao atribuir cargo: ${err.message}`);
