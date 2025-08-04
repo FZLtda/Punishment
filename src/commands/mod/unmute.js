82eb95963f1352266564f9869011f0f61e83c01b
FuncZero <contato@funczero.xyz>
2025-08-04 20:51:32 -0300

    chore: atualiza imports para refletir nova estrutura de pastas utils

--- src/commands/mod/unmute.js
+++ src/commands/mod/unmute.js
@@ -3,8 +3,8 @@
 const { EmbedBuilder } = require('discord.js');
 const { colors, emojis } = require('@config');
 const { sendModLog } = require('@modules/modlog');
-const { sendWarning } = require('@utils/embedWarning');
-const { checkMemberGuard } = require('@utils/memberGuards');
+const { sendWarning } = require('@embeds/embedWarning');
+const { checkMemberGuard } = require('@permissions/memberGuards');
 
 module.exports = {
   name: 'unmute',
