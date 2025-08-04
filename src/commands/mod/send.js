1809d63307bd021d336b335dfd6354235f86493f
FuncZero <contato@funczero.xyz>
2025-08-04 20:48:29 -0300

    chore: atualiza imports para refletir nova estrutura de pastas utils

--- src/commands/mod/send.js
+++ src/commands/mod/send.js
@@ -2,7 +2,7 @@
 
 const { EmbedBuilder, ChannelType } = require('discord.js');
 const { colors, emojis } = require('@config');
-const { sendWarning } = require('@utils/embedWarning');
+const { sendWarning } = require('@embeds/embedWarning');
 const { sendModLog } = require('@modules/modlog');
 
 module.exports = {
