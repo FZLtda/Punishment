fe496ad2878197a20192d2d57fd51edb18ad5adb
FuncZero <contato@funczero.xyz>
2026-02-01 13:44:39 -0300

    feat: configurar cliente Axios para integração com API

---
+++ src/services/apiClient.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+const API_URL = process.env.PUNISHMENT_API_URL;
+const API_KEY = process.env.BOT_API_KEY;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'x-api-key': API_KEY,
+    'Content-Type': 'application/json'
+  },
+  timeout: 5000
+});
+
+module.exports = { api };
