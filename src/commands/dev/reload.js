23c37ed07af0d8f37694cc8da680239374af1acd
FuncZero <contato@funczero.xyz>
2026-02-14 19:47:45 -0300

    fix: correct path to embedWarning

--- src/commands/dev/reload.js
+++ src/commands/dev/reload.js
@@ -3,7 +3,7 @@
 const fs               = require('fs');
 const path             = require('path');
 const { bot, emojis }  = require('@config');
-const { sendWarning }  = require('@embeds/sendWarning');
+const { sendWarning }  = require('@embeds/embedWarning');
 
 module.exports = {
   name: 'reload',
