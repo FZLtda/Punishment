e663d53fda30a8ccba0e31ccb10295966b3aea88
FuncZero <contato@funczero.xyz>
2026-03-29 18:25:50 -0300

    feat: add centralized embeds index file

---
+++ src/utils/embeds/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Central export file for all embed utilities.
+ * Keeps imports clean and makes the embeds layer scalable.
+ */
+
+const banEmbed = require("./banEmbed");
+const kickEmbed = require("./kickEmbed");
+const muteEmbed = require("./muteEmbed");
+const embedError = require("./embedError");
+const embedSuccess = require("./embedSuccess");
+const embedWarning = require("./embedWarning");
+
+module.exports = {
+  ...banEmbed,
+  ...kickEmbed,
+  ...muteEmbed,
+  ...embedError,
+  ...embedSuccess,
+  ...embedWarning,
+};
