cf0c76255989e50d88e65b164bb9666b4365de8e
FuncZero <contato@funczero.xyz>
2026-03-31 23:47:22 -0300

    Add loadContext function to bootstrap process

--- src/core/bootstrap.js
+++ src/core/bootstrap.js
@@ -5,6 +5,7 @@ const { validateEnvironment } = require("@core/environment");
 const { connectMongo } = require("@database");
 
 const { loadCommands } = require("@loadCommands/loader");
+const { loadContext } = require("@loadContext/loader");
 const { loadEvents } = require("@loadEvents/loader");
 const { loadMenus } = require("@loadMenus/loader");
 const { loadModals } = require("@loadModals/loader");
@@ -21,6 +22,7 @@ module.exports = async function bootstrap() {
 
   await Promise.all([
     loadCommands(client),
+    loadContext(client),
     loadEvents(client),
     loadMenus(client),
     loadSlashCommands(client),
