ec9f682bd6ad3b495b4946806449dbe4ab5e18a5
FuncZero <contato@funczero.xyz>
2026-05-11 23:23:27 -0300

    feat: add reason field to unlockuser command

--- src/utils/embeds/createUserUnlockEmbed.js
+++ src/utils/embeds/createUserUnlockEmbed.js
@@ -6,14 +6,14 @@ const { colors, emojis } = require("@config");
 /**
  * @param {import('discord.js').User} moderator
  * @param {import('discord.js').GuildMember} target
+ * @param {string} reason
  */
-function createUserUnlockEmbed(moderator, target) {
+function createUserUnlockEmbed(moderator, target, reason) {
   return new EmbedBuilder()
     .setTitle(`${emojis.unlock} Punição removida`)
     .setColor(colors.green)
-    .setDescription(
-      `${target} (\`${target.id}\`) pode novamente enviar mensagens neste canal.`
-    )
+    .setDescription(`${target} (\`${target.id}\`) pode novamente enviar mensagens neste canal.`)
+    .addFields({ name: "Motivo", value: `\`${reason}\``, inline: true })
     .setThumbnail(target.user.displayAvatarURL({ dynamic: true }))
     .setFooter({
       text: moderator.username,
