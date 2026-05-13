dcd168d057786c644c756a5e862166d4933c3e3b
FuncZero <contato@funczero.xyz>
2026-05-13 09:43:54 -0300

    refactor: centralize checks exports in index file

---
+++ src/middlewares/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const checkCooldown = require("./checkCooldown");
+const checkGlobalBan = require("./checkGlobalBan");
+const checkTerms = require("./checkTerms");
+
+module.exports = {
+  checkCooldown,
+  checkGlobalBan,
+  checkTerms
+};
