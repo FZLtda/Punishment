e7c7589bfd9204a13d626cac663b1313870e36e8
FuncZero <contato@funczero.xyz>
2026-05-13 19:35:26 -0300

    refactor: add centralized index for models to improve import organization

---
+++ src/models/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const ChannelLock = require("./ChannelLock");
+const Giveaway = require("./Giveaway");
+const GlobalBan = require("./GlobalBan");
+const GuildConfig = require("./GuildConfig");
+const GuildSettings = require("./GuildSettings");
+const ModerationAction = require("./ModerationAction");
+const PendingDonor = require("./PendingDonor");
+const TermsAgreement = require("./TermsAgreement");
+const Verification = require("./Verification");
+
+module.exports = {
+  ChannelLock,
+  Giveaway,
+  GlobalBan,
+  GuildConfig,
+  GuildSettings,
+  ModerationAction,
+  PendingDonor,
+  TermsAgreement,
+  Verification,
+};
+
