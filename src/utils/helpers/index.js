0aab6d891596bc31c3cad0fd8b4068de2c280bcb
FuncZero <contato@funczero.xyz>
2026-05-13 15:45:37 -0300

    refactor: centralize utils exports in index file

---
+++ src/utils/helpers/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const formatUsage = require("./formatUsage");
+const helpCategories = require("./helpCategories");
+const prefixManager = require("./prefixManager");
+const responses = require("./responses");
+
+module.exports = {
+  formatUsage,
+  helpCategories,
+  prefixManager,
+  responses,
+};
