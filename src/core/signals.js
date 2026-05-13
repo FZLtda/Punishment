122893a9d0b2f0bfdb7a4e8a0f94bad4bfcbfdc9
FuncZero <contato@funczero.xyz>
2026-05-13 19:20:29 -0300

    refactor: use relative imports inside core modules

--- src/core/signals.js
+++ src/core/signals.js
@@ -1,14 +1,16 @@
 "use strict";
 
 const Logger = require("@logger");
-const { gracefulExit } = require("@core/shutdown");
+const { gracefulExit } = require("./shutdown");
 
 function registerSignalHandlers() {
   ["SIGINT", "SIGTERM"].forEach((signal) => {
     process.on(signal, async () => {
       Logger.warn(`Sinal recebido: ${signal}`);
       await gracefulExit(0);
+      
     });
+    
   });
 }
 
