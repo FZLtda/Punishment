9717d0c007b01c1a631d2e1a3aa77425b599dd2d
FuncZero <contato@funczero.xyz>
2026-05-16 20:56:58 -0300

    chore(sorteios): add index barrel for exports

---
+++ src/tasks/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const iniciarAtribuicaoDeDoadores = require('./atribuirDoadoresPendentes');
+const iniciarVerificacaoSorteios = require('./sorteios');
+
+module.exports = {
+  iniciarAtribuicaoDeDoadores,
+  iniciarVerificacaoSorteios
+};
+
